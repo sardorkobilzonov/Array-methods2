63b119e4775c98175d7a38c6ff68e5422f22561e
sardorkobilzonov <sardorkobilzonov4@gmail.com>
2024-11-19 23:32:26 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,60 @@
+//1-masala
+// let arr = [-5, 3, 8, -2, 4, -1, 0];
+
+// let musbatSon = arr.filter((num) => num > 0);
+
+// console.log(musbatSon);
+//2-masala
+// let sentence1 = "hello world";
+// let sentence2 = "lorem ipsum dollar";
+// let capitalizeFirstLetter = (str) => {
+//   return str
+//     .split(" ")
+//     .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase()) // Har bir so'zning birinchi harfini katta qilib, qolganini kichik qilamiz
+//     .join(" ");
+// };
+
+// console.log(capitalizeFirstLetter(sentence1));
+// console.log(capitalizeFirstLetter(sentence2));
+//3-masala
+// function reverseNum(number) {
+//   return +number.toString().split("").reverse().join("");
+// }
+
+// console.log(reverseNum(1234));
+// console.log(reverseNum(1956));
+
+//4-masala
+
+// let arr = [true, false, true, false, true, false, ];
+
+// let falseValues = arr.filter((value) => value === false);
+
+// console.log(falseValues);
+
+//5-masala
+
+// const arr = [-5, 3, 8, -2, 4, -1, 0];
+
+// let sum = 0;
+
+// arr.filter((num) => num > 0).forEach((num) => (sum += num));
+
+// console.log(sum);
+
+//CODEWARS MISOLLARI
+
+//1-masala
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+//2-masla
+// function evenOrOdd(number) {
+//     if(number % 2 === 0){
+//       return "Even"
+//     }else{
+//       return "Odd"
+//   }
+//   }
